6747b2b49bd9743129676bf697e88f10026c3c3a
Annaaste <anna.dyberg@gmail.com>
2022-02-17 10:38:20 +0100

    Fungerande backend

--- app.js
+++ app.js
@@ -2,15 +2,34 @@ const cors = require("cors")
 const express = require("express")
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded( { extended: true }));
 app.use(cors())
 
-app.get("/", (req ,res)=>{
+app.get("/football", (req ,res)=>{
     headers={"http_status":200, "cache-control": "no-cache"}
-    body={"status": "available"}
+    body=
+    [
+        {
+            "name": "AIK",
+            "points": 9
+        },
+        {
+            "name": "DIF",
+            "points": 6
+        },
+        {
+            "name": "BP",
+            "points": 5
+        },
+        {
+            "name": "HSK",
+            "points": 3
+        }
+    ]
+    res.set('Content-Type', 'application/json')
     res.status(200).send(body)
 })
 
